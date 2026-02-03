8078b2613373c990a24f5ba7a00288351be5fec8
Edward Boileau <edwardboileau@gmail.com>
2026-02-03 18:10:52 +0000

    continued to work on hammer file, need to comment out the thing that stops the wiimote api detecting motionplus.

--- Assets/Scripts/hammer_behaviour.cs
+++ Assets/Scripts/hammer_behaviour.cs
@@ -1,28 +1,73 @@
 using System;
+using UnityEditor;
+using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
+using WiimoteApi;
 
 public class hammer_behaviour : MonoBehaviour
 {
+
+    public Wiimote wiimote;
+
+    static void ConnectWiimote(Wiimote wiimote) {
+         WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
+
+        if(WiimoteManager.HasWiimote()) {
+            //Use the first wiimote: others ignored
+            wiimote = WiimoteManager.Wiimotes[0];
+
+            if (wiimote.RequestIdentifyWiiMotionPlus())
+                {
+                    wiimote.ActivateWiiMotionPlus();
+                    print("connected with wiimotionplus");
+                } else print("Wii remote doesn't have motion plus :((");
+        }
+    }
+    
     static public Quaternion startingRotation;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       //Hammer should be flat with Pitch = 0
-       startingRotation = Quaternion.Euler(new Vector3(270,0,0));
+        //Hammer should start flat with Pitch = 0
+        startingRotation = Quaternion.Euler(new Vector3(270,0,0));
 
-        print("Tap device screen to enable gyroscope!");
+        ConnectWiimote(wiimote);
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {   
-        //start() sadly runs after input is connected,
-        //so I put this here. 
-        //Sort enabling of gyroscopes later with input manager object probably
-        if (Input.touchCount > 0) Input.gyro.enabled = true;
-        
+       
 
-        //Debug.Log(Input.gyro.attitude);
+
+        //As well as making this more efficient, we can probs use the "slow mode" booleans to improve accuracy
+        int ret;
+        do {
+            ret = wiimote.ReadWiimoteData();
+            //not sure re efficiency, this may be v slow and laggy
+            transform.rotation *= Quaternion.Euler(
+                wiimote.MotionPlus.PitchSpeed/95, 
+                wiimote.MotionPlus.YawSpeed/95, 
+                wiimote.MotionPlus.RollSpeed/95);
+        } while (ret > 0); // ReadWiimoteData() returns 0 when nothing is left to read.  
+                // So by doing this we continue to update the Wiimote's attitude until it is "up to date."
+
+
         transform.rotation = Quaternion.Inverse(Input.gyro.attitude * startingRotation);
+
+
+        /*
+        also doesn't work because of input system
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print("Looking for connected controllers");
+            ConnectController();
+            }
+        */
+        
+        
+        
     }
 }
