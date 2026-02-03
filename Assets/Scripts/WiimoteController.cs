3bdb40a4353762ee508631c64fed240e3d2f4393
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-02-03 21:27:30 +0000

    Revert "abstract wiimote code into separate script"
    This reverts commit d8cbf1d34092269dcabffcaa0798c853cfc7eff5.

--- Assets/Scripts/WiimoteController.cs
+++ Assets/Scripts/WiimoteController.cs
@@ -1,99 +1,44 @@
 using UnityEngine;
-using UnityEngine.Assertions;
 using WiimoteApi;
 public class WiimoteController : MonoBehaviour
 {
-    public Wiimote Wiimote { get; private set; }
-    
-    private void ConnectWiimote() {
-        if (WiimoteManager.HasWiimote())
-        {
-            Debug.LogWarning("Attempting to find a Wiimote even though one is already connected!");
-        }
-        
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Wiimote remote;
+    private void InitWiimotes()
+    {
         WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
 
-        if (WiimoteManager.HasWiimote())
-        {
-            Assert.IsTrue(WiimoteManager.Wiimotes.Count == 1, "Only one Wiimote should be connected at a time");
-            //Use the first wiimote: others ignored
-            Wiimote = WiimoteManager.Wiimotes[0];
-
-            //If the wiimote wasn't connected through dolphin, it may still have blinking lights
-            //even though it actually is still connected
-            Wiimote.SendPlayerLED(true, false, false, false);
-
-            if (Wiimote.Type == WiimoteType.WIIMOTEPLUS)
-            {
-                //Running RequestIdentifyWiiMotionPlus() on a Wiimote Plus unfortunately fails,
-                //so we have to skip that check.
-                Debug.Log("Wii Remote Plus detected, skipping Motion Plus check.");
-                Wiimote.ActivateWiiMotionPlus();
-            }
-            else
-            {
-                Wiimote.RequestIdentifyWiiMotionPlus();
-
-                if (Wiimote.wmp_attached)
-                {
-                    Wiimote.ActivateWiiMotionPlus();
-                    Debug.Log("Connected with Wii Motion Plus Extension.");
-                }
-                else
-                {
-                    Debug.LogWarning("Wii remote doesn't have motion plus :(");
-                }
-            }
-            
-            //Default input mode only sends button data, so for accelerometer / gyro data 
-            //we need to request a mode with extension bytes
-            Wiimote.SendDataReportMode(InputDataType.REPORT_EXT21);
-        }
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
-    {
-        //Hammer should start flat with Pitch = 0
-        ConnectWiimote();
-    }
-
-    private void OnDisable()
-    {
-        Debug.Log("Cleaning up Wiimote connections.");
+        remote = WiimoteManager.Wiimotes[0];
+        remote.SendPlayerLED(true, false, false, true);
         
-        //Iterate from the end of the collection to prevent errors from the cleanup function removing each element
-        for (int index = WiimoteManager.Wiimotes.Count - 1; index >= 0; index--)
+    }
+
+    private void FinishedWithWiimotes()
+    {
+        foreach (Wiimote remote in WiimoteManager.Wiimotes)
         {
-            Wiimote remote = WiimoteManager.Wiimotes[index];
             WiimoteManager.Cleanup(remote);
         }
-
-        //Ensure reference to removed wiimote isn't used
-        Wiimote = null;
+    }
+    void Start()
+    {
+        InitWiimotes();
     }
 
-    public void RotateObject(Transform objTransform)
+    private void OnApplicationQuit()
     {
-        //TODO As well as making this more efficient, we can probs use the "slow mode" booleans to improve accuracy
-        int ret;
-        do {
-            Assert.IsTrue(WiimoteManager.HasWiimote(), "A Wiimote must be connected");
-            
-            ret = Wiimote.ReadWiimoteData();
-            //not sure re efficiency, this may be v slow and laggy
-            objTransform.rotation *= Quaternion.Euler(
-                Wiimote.MotionPlus.PitchSpeed/95, 
-                Wiimote.MotionPlus.YawSpeed/95, 
-                Wiimote.MotionPlus.RollSpeed/95);
-        } while (ret > 0); // ReadWiimoteData() returns 0 when nothing is left to read.  
-        // So by doing this we continue to update the Wiimote's attitude until it is "up to date."
+        FinishedWithWiimotes();
     }
 
     // Update is called once per frame
-    private void Update()
-    {   
+    void Update()
+    {
         
-        
+
+        remote.RumbleOn = true; // Enabled Rumble
+        remote.SendStatusInfoRequest(); // Requests Status Report, encodes Rumble into input repo
+        //Thread.Sleep(500); // Wait 0.
+        remote.RumbleOn = false; // Disabled Rumble
+        remote.SendStatusInfoRequest(); // Requests Status Report, encodes Rumble into input report
     }
 }
