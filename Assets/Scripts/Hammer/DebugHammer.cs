65b4222766e7adb01d5185537ae59ccbffa9a2a2
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-02-05 23:06:42 +0000

    adjust hammer script structure and add player prefab
    forgot to add the prefab in previous commit

---
+++ Assets/Scripts/Hammer/DebugHammer.cs
@@ -0,0 +1,38 @@
+using System.Globalization;
+using Hammer;
+using TMPro;
+using UnityEngine;
+using WiimoteApi;
+
+namespace Hammer
+{
+    public class DebugHammer : MonoBehaviour
+    {
+        public TMP_Text pitchSpeedText;
+        public TMP_Text rollSpeedText;
+        public TMP_Text yawSpeedText;
+
+        private Wiimote _wiimote;
+    
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            _wiimote = GetComponent<HammerBehaviour>().Wiimote;
+        }
+
+        public void CalibrateWiiMotionPlus()
+        {
+            Debug.Log("Calibrating WMP values");
+            _wiimote.MotionPlus.SetZeroValues();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            pitchSpeedText.text = $"Pitch Speed: {_wiimote.MotionPlus.PitchSpeed.ToString(CultureInfo.CurrentCulture)}";
+            rollSpeedText.text = $"Roll Speed: {_wiimote.MotionPlus.RollSpeed.ToString(CultureInfo.CurrentCulture)}";
+            yawSpeedText.text = $"Yaw Speed: {_wiimote.MotionPlus.YawSpeed.ToString(CultureInfo.CurrentCulture)}";
+        }
+    }
+}
+
---
+++ Assets/Scripts/Hammer/HammerBehaviour.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Globalization;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using WiimoteApi;
+
+namespace Hammer
+{
+    public class HammerBehaviour : MonoBehaviour
+    {
+
+        public Wiimote Wiimote { get; private set; }
+        
+        //Hammer should start flat with Pitch = 0
+        private readonly Quaternion _startingRotation = Quaternion.Euler(new Vector3(270,0,0));
+
+        void ConnectWiimote() {
+            if (WiimoteManager.HasWiimote())
+            {
+                Debug.LogWarning("Attempting to find a Wiimote even though one is already connected!");
+            }
+            
+            WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
+
+            if (WiimoteManager.HasWiimote())
+            {
+                Assert.IsTrue(WiimoteManager.Wiimotes.Count == 1, "Only one Wiimote should be connected at a time");
+                //Use the first wiimote: others ignored
+                Wiimote = WiimoteManager.Wiimotes[0];
+
+                //If the wiimote wasn't connected through dolphin, it may still have blinking lights
+                //even though it actually is still connected
+                Wiimote.SendPlayerLED(true, false, false, true);
+
+                if (Wiimote.Type == WiimoteType.WIIMOTEPLUS)
+                {
+                    //Running RequestIdentifyWiiMotionPlus() on a Wiimote Plus unfortunately fails,
+                    //so we have to skip that check.
+                    Debug.Log("Wii Remote Plus detected, skipping Motion Plus check.");
+                    Wiimote.ActivateWiiMotionPlus();
+                }
+                else
+                {
+                    Wiimote.RequestIdentifyWiiMotionPlus();
+
+                    if (Wiimote.wmp_attached)
+                    {
+                        Wiimote.ActivateWiiMotionPlus();
+                        Debug.Log("Connected with Wii Motion Plus Extension.");
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Wii remote doesn't have motion plus :(");
+                    }
+                }
+                
+                //Default input mode only sends button data, so for accelerometer / gyro data 
+                //we need to request a mode with extension bytes
+                Wiimote.SendDataReportMode(InputDataType.REPORT_BUTTONS_EXT19);
+            }
+        }
+        
+        private void CleanupWiimotes()
+        {
+            Debug.Log("Cleaning up Wiimote connections.");
+            
+            //Iterate from the end of the collection to prevent errors from the cleanup function removing each element
+            for (int index = WiimoteManager.Wiimotes.Count - 1; index >= 0; index--)
+            {
+                Wiimote remote = WiimoteManager.Wiimotes[index];
+                // remote.SendPlayerLED(true, false, false, false);
+                WiimoteManager.Cleanup(remote);
+            }
+
+            //Ensure reference to removed wiimote isn't used
+            Wiimote = null;
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Awake()
+        {
+            ConnectWiimote();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            //TODO As well as making this more efficient, we can probs use the "slow mode" booleans to improve accuracy
+            int ret;
+            do {
+                Assert.IsTrue(WiimoteManager.HasWiimote(), "A Wiimote must be connected");
+                
+                ret = Wiimote.ReadWiimoteData();
+                
+                //not sure re efficiency, this may be v slow and laggy
+                transform.Rotate( new Vector3(
+                    Wiimote.MotionPlus.PitchSpeed, 
+                    Wiimote.MotionPlus.YawSpeed, 
+                    Wiimote.MotionPlus.RollSpeed) / 95f);
+            } while (ret > 0); // ReadWiimoteData() returns 0 when nothing is left to read.  
+                    // So by doing this we continue to update the Wiimote's attitude until it is "up to date."
+
+            if (Wiimote.Button.a)
+            {
+                Debug.Log("Calibrating WMP values");
+                Wiimote.MotionPlus.SetZeroValues();
+            }
+
+            //Unity Remote
+            //transform.rotation = Quaternion.Inverse(Input.gyro.attitude * _startingRotation);
+
+
+            /*
+            also doesn't work because of input system
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                print("Looking for connected controllers");
+                ConnectController();
+                }
+            */
+            
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+
+        void OnDisable()
+        {
+            CleanupWiimotes();
+        }
+    }
+
+}
