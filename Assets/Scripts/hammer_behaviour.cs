7860b7a836d6688a37818418a20925477c4eb472
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-02-05 23:03:34 +0000

    add hammer to main scene
    lol gameplay isn't very good but i think we can show this off
    
    for now i made it so that there is a rigidbody on the hammer but physics basicaly doesnt effect it(to my knowledge). there's probably a better way to do it though to achieve having collision checks without the physics. tried using a trigger but it didn't work

--- Assets/Scripts/hammer_behaviour.cs
+++ Assets/Scripts/hammer_behaviour.cs
@@ -122,6 +122,14 @@ public class hammer_behaviour : MonoBehaviour
         
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
     void OnDisable()
     {
         CleanupWiimotes();
