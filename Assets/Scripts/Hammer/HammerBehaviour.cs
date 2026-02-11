4c224c28f7c83ed73b9c1cebf86910d70478a8ca
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-02-11 11:06:42 +0000

    extract UseWiimoteData function
    i think splitting up accel and gyro into separate functions wouldn't work that well because it's all one read

--- Assets/Scripts/Hammer/HammerBehaviour.cs
+++ Assets/Scripts/Hammer/HammerBehaviour.cs
@@ -21,17 +21,17 @@ namespace Hammer
             Phone
         }
         
-        private InputDevice _device = InputDevice.Phone;
+        private InputDevice _inputDevice = InputDevice.Phone;
 
         public Wiimote Wiimote { get; private set; }
         
         //Hammer should start flat with Pitch = 0
         public Quaternion StartingRotation { get; private set; }
 
-        private Quaternion attitude;
+        private Quaternion _attitude;
 
 
-        bool ConnectWiimote() {
+        private bool ConnectWiimote() {
             if (WiimoteManager.HasWiimote())
             {
                 Debug.LogWarning("Attempting to find a Wiimote even though one is already connected!");
@@ -99,91 +99,99 @@ namespace Hammer
         }
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private void Start()
         {
             
         }
 
         //Called once before start when the game starts
-        void Awake()
+        private void Awake()
         {
             StartingRotation = transform.rotation;
             bool wiimoteConnected = ConnectWiimote();
             if (wiimoteConnected)
             {
-                _device = InputDevice.Wiimote;
+                _inputDevice = InputDevice.Wiimote;
             }
             else
             {
-                _device = InputDevice.Phone;
+                _inputDevice = InputDevice.Phone;
             }
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-            if (_device == InputDevice.Wiimote)
+            if (_inputDevice == InputDevice.Wiimote)
             {
-                Assert.IsTrue(WiimoteManager.HasWiimote(), "A Wiimote must be connected");
-
-                //TODO As well as making this more efficient, we can probs use the "slow mode" booleans to improve accuracy
-                int ret;
-                Vector3 gyroOffset = Vector3.zero;
-                Vector3 accelOffset = Vector3.zero;
-
-                do {
-                    ret = Wiimote.ReadWiimoteData();
-
-                    //ACCELEROMETER
-                    
-                    Vector3 accelDataForFrameTest = new Vector3(
-                        Wiimote.Accel.GetCalibratedAccelData()[0],
-                        Wiimote.Accel.GetCalibratedAccelData()[1],
-                        Wiimote.Accel.GetCalibratedAccelData()[2]);
-                    print("Accel data: "+accelDataForFrameTest);
-                    accelOffset += accelDataForFrameTest;
-                    
-                    //this is a test basically
-
-                    //GYROSCOPE
-                    //add all detected rotations throughout the frame to gyroOffset
-                    //we should integrate this! would give accurate total rotation
-                    if (ret > 0 && Wiimote.current_ext == ExtensionController.MOTIONPLUS) {
-                        gyroOffset += new Vector3(  -Wiimote.MotionPlus.PitchSpeed,
-                                                        -Wiimote.MotionPlus.RollSpeed,
-                                                        -Wiimote.MotionPlus.YawSpeed);
-                    }
-                } while (ret > 0);
-
-                gyroOffset /= 95f; //divide by 95 because of the average rate of sending messages of the wiimote is 95Hz
-                                //and speeds of rotations are sent in degrees per second (i think!)
-                                //would be cool to actually count the number of updates per second but I'm not sure how. 
-                                //i think that this is completely wrong. nothing like 95 messages sent per frame. but idk
-                                //oh wait yeah ofc its wrong, we are not 1 frame per second!! 
-
-
-
-                // ReadWiimoteData() returns 0 when nothing is left to read.
-                // So by doing this we continue to update the Wiimote's attitude until it is "up to date."
-
-                transform.Rotate(gyroOffset, Space.Self);
-                
-                //print("Total accel offset for frame: "+accelOffset);
-                //transform.Translate(accelOffset/95f, Space.Self);
-                //just using accel values/100 for translation - makes no sense but testing!
+                UseWiimoteData();
             }
             //Unity Remote
             else
             {
-                Assert.IsTrue(_device == InputDevice.Phone, "Only Wiimote and Phone inputs are handled!");
+                Assert.IsTrue(_inputDevice == InputDevice.Phone, "Only Wiimote and Phone inputs are handled!");
                 
                 //start() sadly runs after input is connected,
                 //so I put this here. 
                 //Sort enabling of gyroscopes later with input manager object probably
-                if (Input.touchCount > 0) Input.gyro.enabled = true;
+                if (Input.touchCount > 0)
+                {
+                    Input.gyro.enabled = true;
+                }
 
                 transform.rotation = Quaternion.Inverse(Input.gyro.attitude * StartingRotation);
             }
+        }
+
+        private void UseWiimoteData()
+        {
+            Assert.IsTrue(WiimoteManager.HasWiimote(), "A Wiimote must be connected");
+
+            //TODO As well as making this more efficient, we can probs use the "slow mode" booleans to improve accuracy
+            int ret;
+            Vector3 gyroOffset = Vector3.zero;
+            Vector3 accelOffset = Vector3.zero;
+
+            do {
+                ret = Wiimote.ReadWiimoteData();
+
+                //ACCELEROMETER
+                
+                Vector3 accelDataForFrameTest = new Vector3(
+                    Wiimote.Accel.GetCalibratedAccelData()[0],
+                    Wiimote.Accel.GetCalibratedAccelData()[1],
+                    Wiimote.Accel.GetCalibratedAccelData()[2]);
+                print("Accel data: "+accelDataForFrameTest);
+                accelOffset += accelDataForFrameTest;
+                
+                //this is a test basically
+
+                //GYROSCOPE
+                //add all detected rotations throughout the frame to gyroOffset
+                //we should integrate this! would give accurate total rotation
+                if (ret > 0 && Wiimote.current_ext == ExtensionController.MOTIONPLUS) {
+                    gyroOffset += new Vector3(  -Wiimote.MotionPlus.PitchSpeed,
+                                                    -Wiimote.MotionPlus.RollSpeed,
+                                                    -Wiimote.MotionPlus.YawSpeed);
+                }
+            } while (ret > 0);
+
+            gyroOffset /= 95f; //divide by 95 because of the average rate of sending messages of the wiimote is 95Hz
+                            //and speeds of rotations are sent in degrees per second (i think!)
+                            //would be cool to actually count the number of updates per second but I'm not sure how. 
+                            //i think that this is completely wrong. nothing like 95 messages sent per frame. but idk
+                            //oh wait yeah ofc its wrong, we are not 1 frame per second!! 
+
+
+
+            // ReadWiimoteData() returns 0 when nothing is left to read.
+            // So by doing this we continue to update the Wiimote's attitude until it is "up to date."
+
+            transform.Rotate(gyroOffset, Space.Self);
+            
+            //print("Total accel offset for frame: "+accelOffset);
+            //transform.Translate(accelOffset/95f, Space.Self);
+            //just using accel values/100 for translation - makes no sense but testing!
         }
 
         public void OnCollisionEnter(Collision collision)
