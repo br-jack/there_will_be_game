e04575bb893085b85e0571d5aa069b28e848e084
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-02-14 19:46:57 +0000

    Make scene manager a gameobject with dont destory on load
    this lets its methods be called with unity event
    
    had to create a tag for this though, not sure if thats the best thign t o do
    
    used this as reference:
    
    https://docs.unity3d.com/ScriptReference/Object.DontDestroyOnLoad.html

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,7 +1,22 @@
-﻿using UnityEngine.SceneManagement;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public static class SceneController
+public class SceneController : MonoBehaviour
 {
+    private void Awake()
+    {
+        //Make sure only one scene manager is loaded at a time
+        //(not sure if there's a better way to do this without using a tag)
+        GameObject[] sceneManagers = GameObject.FindGameObjectsWithTag("SceneController");
+        if (sceneManagers.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
     public static void LoadMenu()
     {
         SceneManager.LoadScene("Menu");
