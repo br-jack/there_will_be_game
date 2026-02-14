541dc2fe63d40293c81281afcb7a87b96da346ed
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-02-14 19:57:09 +0000

    Use prefab rather than dontdestroyonload for SceneController.cs
    i think its more transparent this way because it doesnt really have state anyway
    
    https://discussions.unity.com/t/should-i-use-dontdestroyonload-or-prefab-for-player/212716
    
    https://www.reddit.com/r/Unity3D/comments/1f1pijg/should_i_use_dontdestroyonload_on_player_and/

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,19 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    private void Awake()
-    {
-        //Make sure only one scene manager is loaded at a time
-        //(not sure if there's a better way to do this without using a tag)
-        GameObject[] sceneManagers = GameObject.FindGameObjectsWithTag("SceneController");
-        if (sceneManagers.Length > 1)
-        {
-            Destroy(this.gameObject);
-        }
-        
-        DontDestroyOnLoad(gameObject);
-    }
-
     public static void LoadMenu()
     {
         SceneManager.LoadScene("Menu");
