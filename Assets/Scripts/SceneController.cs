01f62848cf7e609d8efa89fa525514fc73401494
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-02-14 19:40:17 +0000

    try using scene controller, but i guess it doesnt work with unity events
    tried looking for best practise on how to load  a scene from a butotn but i coudlnt find anything. i guess the best thing to do in cases like that is just to try something and see whether it works. in this case this doesn't.
    
    https://stackoverflow.com/questions/63495806/unity-dontdestroyonload-or-public-static-variable
    
    https://www.reddit.com/r/Unity3D/comments/1362sci/dontdestroyonload_vs_public_static_instance/
    
    https://unity.com/how-to/architect-game-code-scriptable-objects
    
    https://gamedev.stackexchange.com/questions/32861/whats-the-proper-way-to-use-static-objects-and-singletons-in-unity
    
    https://stackoverflow.com/questions/69540531/how-to-pass-a-gameobject-through-a-non-static-class-in-unity

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+
+public static class SceneController
+{
+    public static void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    
+    public static void LoadGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    
+    public static void LoadCalibration()
+    {
+        SceneManager.LoadScene("hammerTest");
+    }
+}
