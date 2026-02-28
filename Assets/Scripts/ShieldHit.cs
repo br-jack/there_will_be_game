68825ea48fb60f2ec0ffc1a82e2c04145944dc4a
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-02-28 19:37:10 +0000

    remove unused audiosource variable
    i think the shield hit audio is played elsewhere

--- Assets/Scripts/ShieldHit.cs
+++ Assets/Scripts/ShieldHit.cs
@@ -3,16 +3,9 @@ using UnityEngine;
 public class ShieldHit : MonoBehaviour
 {
     public float knockbackForce = 20f;
-    public AudioSource audioSource;
 
     void OnTriggerEnter(Collider other)
     {
-           audioSource = GetComponent<AudioSource>();
-
-        if (audioSource == null)
-        {
-            Debug.LogError("man there's no audio source");
-        }
         AttackHitbox attack = other.GetComponent<AttackHitbox>();
         if (attack == null)
         {
