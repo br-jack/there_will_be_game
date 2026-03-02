dcb198f5cc265c3ced8a7fa9deeefee7d94aae2b
HaSRoberts <tu23666@bristol.ac.uk>
2026-03-02 17:56:17 +0000

    messed around with different maps. now focusing on getting the horse to not clip into walls

--- Assets/Scripts/HorseMovement.cs
+++ Assets/Scripts/HorseMovement.cs
@@ -152,7 +152,8 @@ public class HorseMovement : MonoBehaviour
         
 
         Vector3 forwardMovement = transform.forward * (_currentSpeed * Time.fixedDeltaTime); 
-        
+        _//rb.linearVelocity = _rb.linearVelocity + transform.forward; //#Shay: doing this fixes clipping into walls but breaks everything else.
+        //Looking into another way to get around it
         _rb.MovePosition(_rb.position + forwardMovement);
     }
 }
