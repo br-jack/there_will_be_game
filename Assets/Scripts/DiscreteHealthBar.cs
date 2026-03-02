9dacafb07c184888e5f5161993da25299907317e
onettonett <157602827+onettonett@users.noreply.github.com>
2026-03-02 15:13:37 +0000

    Before seperating the Health.cs system into PlayerLives and PlayerHealth!

--- Assets/Scripts/DiscreteHealthBar.cs
+++ Assets/Scripts/DiscreteHealthBar.cs
@@ -2,22 +2,41 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
+/*
+Responsible for displaying the current player health.
+When "Testing the discrete bar" is NOT checked, this is connected to the player's health.
+*/
+
 public class DiscreteHealthBar : MonoBehaviour
 {
-    [SerializeField] private GameObject healthIconPrefab; // Assign a prefab (e.g., heart image) in Inspector
-    [SerializeField] private Transform iconContainer;     // Assign a UI container (e.g., empty GameObject with HorizontalLayoutGroup)
+    [SerializeField] private GameObject healthIconPrefab;
+    [SerializeField] private Transform iconContainer;   
 
     private List<GameObject> icons = new List<GameObject>();
 
-    public void DisplayHealth(int currentHealth, int maxHealth)
+    private void Awake()
+    {
+        if (iconContainer == null) iconContainer = transform;
+        
+        // Record any existing health icons on the scene (prevents extra icons being added)
+        icons.Clear();
+        for (int i = 0; i < iconContainer.childCount; i++)
+        {
+            icons.Add(iconContainer.GetChild(i).gameObject);
+        }
+    }
+
+    public void DisplayHealth(int cur, int max) // pass in current health and maximum possible health as parameters
     {
         // Ensure correct number of icons
-        while (icons.Count < maxHealth)
+        while (icons.Count < max)
         {
             GameObject icon = Instantiate(healthIconPrefab, iconContainer);
             icons.Add(icon);
         }
-        while (icons.Count > maxHealth)
+
+        // Ensures the number of icons NEVER exceeds the maximum by repeatedly deleting the last icon
+        while (icons.Count > max)
         {
             Destroy(icons[icons.Count - 1]);
             icons.RemoveAt(icons.Count - 1);
@@ -26,7 +45,7 @@ public class DiscreteHealthBar : MonoBehaviour
         // Show/hide icons based on current health
         for (int i = 0; i < icons.Count; i++)
         {
-            icons[i].SetActive(i < currentHealth);
+            icons[i].SetActive(i < cur);
         }
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,12 +5,17 @@ public class Health : MonoBehaviour
     //TODO make setter private but still serialize
     public int lives = 4;
     public int maxLives = 4;
-    public ContinuousHealthBar healthbar;
+    public DiscreteHealthBar discreteBar;
+    public ContinuousBar continuousBar;
 
     public void UpdateHealthDisplay()
     {
-        float health = lives/maxLives;
-        healthbar.DisplayHealth(health);
+        if (discreteBar != null) discreteBar.DisplayHealth(lives, maxLives);
+        /*
+        One of these 2 lines should be used to display the health on either the discrete or continuos bar:
+        1. if (discreteBar != null) discreteBar.DisplayHealth(lives, maxLives);
+        2. if (continuousBar != null) continuousBar.DisplayHealth(lives, maxLives);
+        */
     }
     
     public void LoseLife()
@@ -33,6 +38,6 @@ public class Health : MonoBehaviour
     void Start()
     {
         lives = maxLives;
-        healthbar.DisplayMaxHealth();
+        UpdateHealthDisplay();
     }
 }
