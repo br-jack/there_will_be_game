33c999692c5525316ebdfc1c4b46fc052e33096c
Aravind Pillai <iv23317@bristol.ac.uk>
2026-03-04 23:16:02 +0000

    Enemy knockback force now depends on velocity

--- Assets/Scripts/AttackHitbox.cs
+++ Assets/Scripts/AttackHitbox.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class AttackHitbox : MonoBehaviour
 {
-    public float knockbackForce = 20f;
+    public float baseKnockbackForce = 20f;
+    public float velocityKnockbackMultiplier = 2f;
+    
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponentInParent<Rigidbody>();
+    }
+
+    public float GetKnockbackForce()
+    {
+        if (_rb == null)
+        {
+            return baseKnockbackForce;
+        }
+
+        float speed = _rb.linearVelocity.magnitude;
+        
+        // Now calculates knockback based on the speed of approach
+        float knockback = baseKnockbackForce + (speed * velocityKnockbackMultiplier);
+        
+        return knockback;
+    }
 }
