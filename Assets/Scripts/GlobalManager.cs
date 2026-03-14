7a839154019277b2b9de8c0611f9b96c524805f0
onettonett <157602827+onettonett@users.noreply.github.com>
2026-03-14 12:44:46 +0000

    SetState function with valid state transitions implemented.

--- Assets/Scripts/GlobalManager.cs
+++ Assets/Scripts/GlobalManager.cs
@@ -40,4 +40,65 @@ public class GlobalManager : MonoBehaviour
             ElapsedRunTime += Time.deltaTime;
         }
     }
+
+    public void OnEnable()
+    {
+        SceneManager.sceneLoaded += HandleSceneLoaded;
+    }
+
+    public void OnDisable()
+    {
+        SceneManager.sceneLoaded -= HandleSceneLoaded;
+    }
+
+    public void SetState(GameState next)
+    {
+        bool isValid = (curState, next) switch
+        {
+            // Valid Transitions from Running
+            (GameState.Playing, GameState.Paused) => true,
+            (GameState.Playing, GameState.GameOver) => true,
+            // Valid Transitions from Paused
+            (GameState.Paused, GameState.Playing) => true,
+            (GameState.Paused, GameState.Menu) => true,
+            // Valid Transitions from GameOver
+            (GameState.GameOver, GameState.BeforePlay) => true,
+            (GameState.GameOver, GameState.Menu) => true,
+            // Valid Transitions from Menu
+            (GameState.Menu, GameState.BeforePlay) => true,
+            (GameState.Menu, GameState.Calibration) => true,
+            // Valid Transitions from Calibration
+            (GameState.Calibration, GameState.Menu) => true,
+            // Valid Transitions from PreRun
+            (GameState.BeforePlay, GameState.Running) => true,
+            _ => false
+        };
+
+        if (!isValid)
+        {
+            Debug.LogWarning($"{CurrentState} -> {next} is an invalid game state transition.");
+            return;
+        }
+
+        GameState prev = curState;
+        curState = next;
+
+    }
+
+    private void NewSceneJustLoaded(Scene scene, LoadSceneMode mode)
+    {
+        switch (scene.name)
+        {
+            case "MainScene":
+                SetState(GameState.PreRun);
+                break;
+            case "MainMenu":
+                ForceState(GameState.Menu);
+                break;
+            case "hammerTest":
+                ForceState(GameState.calibration);
+                break;
+        }
+    }
+
 }
