193596713b012073a3cf9cb7f1bd073e451d913c
br-jack <brewer.j-ack@protonmail.com>
2026-03-17 17:29:28 +0000

    switch to serial

--- Arduino/serialIMU/serialIMU.ino
+++ Arduino/serialIMU/serialIMU.ino
@@ -23,55 +23,49 @@ void setup(void) {
   //Without this inital delay it will usually never connect until you press the reset button
   delay(100);
 
-  // Open serial communications and wait for port to open:
-  // Serial.begin(19200);
-  // while (!Serial) {
-  //   ; // wait for serial port to connect. Needed for native USB port only
-  // }
+//  / Open serial communications and wait for port to open:
+   Serial.begin(115200);
+   while (!Serial) {
+     ; // wait for serial port to connect. Needed for native USB port only
+   }
 
-  // BT.begin(9600);
-  // BT.print("AT+ROLE0");
-  // BT.end();
-  BT.begin(115200);
-
-  while (!BT) delay(5);
-  BT.println("Adafruit BNO08x test!");
+  Serial.println("Adafruit BNO08x test!");
 
   // Try to initialize!
   if (!bno08x.begin_I2C()) {
     //if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
     //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
-    BT.println("Failed to find BNO08x chip");
+    Serial.println("Failed to find BNO08x chip");
     while (1) { delay(5); }
   }
-  BT.println("BNO08x Found!");
+  Serial.println("BNO08x Found!");
 
   for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
-    BT.print("Part ");
-    BT.print(bno08x.prodIds.entry[n].swPartNumber);
-    BT.print(": Version :");
-    BT.print(bno08x.prodIds.entry[n].swVersionMajor);
-    BT.print(".");
-    BT.print(bno08x.prodIds.entry[n].swVersionMinor);
-    BT.print(".");
-    BT.print(bno08x.prodIds.entry[n].swVersionPatch);
-    BT.print(" Build ");
-    BT.println(bno08x.prodIds.entry[n].swBuildNumber);
+    Serial.print("Part ");
+    Serial.print(bno08x.prodIds.entry[n].swPartNumber);
+    Serial.print(": Version :");
+    Serial.print(bno08x.prodIds.entry[n].swVersionMajor);
+    Serial.print(".");
+    Serial.print(bno08x.prodIds.entry[n].swVersionMinor);
+    Serial.print(".");
+    Serial.print(bno08x.prodIds.entry[n].swVersionPatch);
+    Serial.print(" Build ");
+    Serial.println(bno08x.prodIds.entry[n].swBuildNumber);
   }
   setReports();
 
-  BT.println("Reading events");
+  Serial.println("Reading events");
   delay(100);
 }
 
 // Here is where you define the sensor outputs you want to receive
 void setReports(void) {
-  BT.println("Setting desired reports");
+  Serial.println("Setting desired reports");
   if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
-    BT.println("Could not enable game vector");
+    Serial.println("Could not enable game vector");
   }
   if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION, 20000)) {
-    BT.println("Could not enable linear acceleration");
+    Serial.println("Could not enable linear acceleration");
   }
 }
 
@@ -79,16 +73,9 @@ void setReports(void) {
 void loop() {  // run over and over
 
   delay(5);
-
-  // if (BT.available()) {
-  //   Serial.write(BT.read());
-  // }
-  // if (Serial.available()) {
-  //   BT.write(Serial.read());
-  // }
-
+  
   if (bno08x.wasReset()) {
-    BT.print("sensor was reset ");
+    Serial.print("sensor was reset ");
     setReports();
   }
 
@@ -110,7 +97,7 @@ void loop() {  // run over and over
         q.concat(sensorValue.un.gameRotationVector.k);
 
 
-        BT.println(q);
+        Serial.println(q);
         break;
       }
 
@@ -124,7 +111,7 @@ void loop() {  // run over and over
         q.concat(sensorValue.un.linearAcceleration.y);
         q.concat(":");
         q.concat(sensorValue.un.linearAcceleration.z);
-        BT.println(q);
+        Serial.println(q);
         break;
       }
   }
