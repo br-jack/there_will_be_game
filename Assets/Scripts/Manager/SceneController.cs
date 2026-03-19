ff89ece8b49dd72690290d65d141938f21a0f90a
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-03-19 23:37:33 +0000

    try moving restart binding to scenecontroller, but it doesn't seem to work for some reason
    https://docs.unity3d.com/Packages/com.unity.inputsystem@1.19/manual/RespondingToActions.html#action-callbacks
    
    not sure this is the best approach anyway, it hides the keybindings from the main keymap

--- Assets/Scripts/Manager/SceneController.cs
+++ Assets/Scripts/Manager/SceneController.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private InputAction restartAction;
+
+    void Awake()
+    {
+        restartAction.performed += context => ReloadScene();
+    }
+    
     public static void ExitGame()
     {
 #if UNITY_EDITOR
@@ -15,6 +23,11 @@ public class SceneController : MonoBehaviour
     public static void LoadMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public static void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     
     public static void LoadGame()
