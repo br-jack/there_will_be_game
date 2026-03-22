e07124849d521de9e983f39bfe6438a79e3b94a3
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-03-22 10:32:22 +0000

    try moving some imu reading and setup code to imucontroller, there's only one problem
    can't call globalmanager directly from non mono behavior script. i guess i could find it wiht tag, but i wonder if theres a better way
    
    https://www.reddit.com/r/Unity3D/comments/1h8zrhu/proper_way_to_handle_variables_that_need_to_be/
    
    https://discussions.unity.com/t/get-reference-to-gameobject-from-non-monobehaviour-class/678513/6
    
    https://www.reddit.com/r/Unity3D/comments/5pb6bp/what_is_the_best_way_of_using_nonmonobehaviour/

--- Assets/Scripts/Hammer/HammerBehaviour.cs
+++ Assets/Scripts/Hammer/HammerBehaviour.cs
@@ -10,13 +10,6 @@ namespace Hammer
     public class HammerBehaviour : MonoBehaviour
     {
 
-        Quaternion gameRotationVector;
-        Vector3 frameAcceleration;
-        SerialPort stream;
-        private Thread ioThread;
-        private bool running;
-        private ConcurrentQueue<string> dataQueue = new ConcurrentQueue<string>();
-
         [SerializeField] float extension;
         float extensionVelocity;
         [SerializeField] float k = 20f;
@@ -29,8 +22,6 @@ namespace Hammer
         private float momentum = 0;
 
         [SerializeField] Transform pivotTransform;
-        private bool portOpen = false;
-        private readonly int timeoutMs = 50;
 
         public Rigidbody rigidBody;
 
@@ -62,120 +53,11 @@ namespace Hammer
             ioThread.Start();
         }
 
-
-        private void Connect()
-        {
-            try
-            {
-                stream = new SerialPort(GlobalManager.Instance.port, 115200)
-                {
-                    ReadTimeout = timeoutMs
-                };
-
-                stream.DtrEnable = true;
-                stream.Open();
-                stream.ReadTimeout = timeoutMs;
-                portOpen = true;
-                // if youre connected but not getting any data you may have another serial monitor open for this port
-                Debug.Log("Connected (allegedly)");
-            }
-            catch (System.Exception e)
-            {
-                portOpen = false;
-                Debug.LogWarning("Failed to connect to port: ");
-                Debug.LogWarning(e);
-            }
-        }
-
-
-        private void IOThreadLoop()
-        {
-            try
-            {
-                while (running)
-                {
-                    string recievedData = null;
-                    try
-                    {
-                        recievedData = stream.ReadLine();
-                        dataQueue.Enqueue(recievedData);
-                    }
-                    catch (Exception ex)
-                    {
-                        //Seems to cause a memory leak, so only enable this when debugging Bluetooth
-                        // Debug.LogWarning($"Error reading data: {ex.Message}");
-                    }
-
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError($"[IO Thread] Error: {ex.Message}");
-            }
-        }
-
         public void CalibrateHammer()
         {
             GlobalManager.Instance.CalibrationQuaternion = Quaternion.Inverse(gameRotationVector);
 
         }
-
-        void ParseStream()
-        {
-            while (dataQueue.TryDequeue(out string data))
-            {
-                Debug.Log($"[Main Thread] Received: {data}");
-                string[] parsedData = data.Trim().Split(':');
-
-
-                if (parsedData[0] == "a")
-                {
-                    try
-                    {
-                        Vector3 acceleration = new(
-                                                float.Parse(parsedData[3]),
-                                                float.Parse(parsedData[1]),
-                                                float.Parse(parsedData[2])
-                                                );
-                        // TODO change to impulse or something (persist across frames)
-                        if (acceleration.magnitude > frameAcceleration.magnitude) frameAcceleration = acceleration;
-
-                    }
-                    catch
-                    {
-                        Debug.LogWarning("Incorrect acceleration format.");
-                    }
-
-                }
-
-                else if (parsedData[0] == "q")
-                {
-                    try
-                    {
-                        //Quaternion possibleQuaternion = new Quaternion(-float.Parse(parsedData[3]),
-                        //    -float.Parse(parsedData[4]),
-                        //    float.Parse(parsedData[2]),
-                        //    float.Parse(parsedData[1]));
-                        Quaternion possibleQuaternion = new Quaternion(float.Parse(parsedData[2]),
-                            -float.Parse(parsedData[4]),
-                            float.Parse(parsedData[3]),
-                            float.Parse(parsedData[1]));
-                        gameRotationVector = possibleQuaternion;
-
-                    }
-                    catch
-                    {
-                        Debug.LogWarning("Incorrect quaternion format.");
-
-                    }
-
-
-                }
-            }
-
-        }
-
-
 
         void UpdateRotation()
         {
