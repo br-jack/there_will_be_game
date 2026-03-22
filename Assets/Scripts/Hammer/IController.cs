3a1ca3770c1bc55a25ad881265c3b12f8aef839f
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-03-22 10:22:27 +0000

    skeleeton for possible interface implementation
    controller classes arent monobehavior but created an update function that could be called from hammerbehaviour's update function. not sure if that's good practise, though.

---
+++ Assets/Scripts/Hammer/IController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Hammer
+{
+    public interface IController
+    {
+        public void Connect();
+        
+        public void Update();
+
+        public Vector3 GetAttitude();
+
+        public Vector3 GetAcceleration();
+
+        public void Cleanup();
+    }
+}
---
+++ Assets/Scripts/Hammer/IMUController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Hammer
+{
+    public class IMUController : IController
+    {
+        public void Connect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector3 GetAttitude()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector3 GetAcceleration()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Cleanup()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
