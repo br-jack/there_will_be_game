d8ab2109e93b0263319d585864e8192bbc713b28
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-03-22 10:37:18 +0000

    hmm maybe have searchports be in imucontroller and the contorller itself being global?
    not sure tbh

--- Assets/Scripts/Hammer/IMUController.cs
+++ Assets/Scripts/Hammer/IMUController.cs
@@ -20,11 +20,60 @@ namespace Hammer
         private bool portOpen = false;
         private readonly int timeoutMs = 50;
         
+        public string port = null;
+        
+        public void SearchPorts()
+        {
+            try
+            {
+                if (Application.platform.Equals(RuntimePlatform.OSXEditor) || Application.platform.Equals(RuntimePlatform.OSXPlayer))
+                {
+                    port = "/dev/cu.usbmodem101";
+                }
+
+                if (Application.platform.Equals(RuntimePlatform.LinuxPlayer) || Application.platform.Equals(RuntimePlatform.LinuxServer))
+                {
+                    port = "/dev/ttyACM0";
+                }
+
+                if (Application.platform.Equals(RuntimePlatform.WindowsEditor) || Application.platform.Equals(RuntimePlatform.WindowsPlayer))
+                {
+
+                    try
+                    {
+                        string[] portNames = SerialPort.GetPortNames();
+
+                        if (portNames.Length < 2)
+                        {
+                            Console.WriteLine("No COM ports found.");
+                            return;
+                        }
+
+                        if (!string.IsNullOrEmpty(portNames[1]))
+                        {
+                            port = portNames[1];
+                            Debug.Log(port);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error scanning COM ports: {ex.Message}");
+                    }
+
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning("Failed to find port: ");
+                Debug.LogWarning(e);
+            }
+        }
+        
         public void Connect()
         {
             try
             {
-                stream = new SerialPort(GlobalManager.Instance.port, 115200)
+                stream = new SerialPort(port, 115200)
                 {
                     ReadTimeout = timeoutMs
                 };
