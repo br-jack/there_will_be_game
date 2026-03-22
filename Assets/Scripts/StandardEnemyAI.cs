255877ca6bb297627f31dc38a3cdd0a3fd2d8a7a
onettonett <157602827+onettonett@users.noreply.github.com>
2026-03-22 01:12:57 +0000

    movement variables added

--- Assets/Scripts/StandardEnemyAI.cs
+++ Assets/Scripts/StandardEnemyAI.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
-
+public struct EnemyAttack
 public class StandardEnemyAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject shield;
+    [Header("Movement")]
+    [SerializeField] private float speed = 5f;
+
+    // The distance the Enemy stops at the player should be less than the player's attack range.
+    [SerializeField] private float stopFromPlayerDistance = 1.5f;
+    [SerializeField] private float smoothVelocity = 0.35f;
+    [SerializeField] private float rotationSpeed = 8f;
+    
+
     void Start()
     {
         
