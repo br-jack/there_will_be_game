65213c52b1b5778026d540091a00e723c4643275
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-03-23 12:12:19 +0000

    hmm even a timer doesn't work
    might just leave it as it was before tbh.

--- Arduino/hub/hub_mega/hub_mega.ino
+++ Arduino/hub/hub_mega/hub_mega.ino
@@ -1,3 +1,5 @@
+int serialTimer = 0;
+
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
@@ -11,11 +13,28 @@ void setup() {
   }
   // BT.print("AT+ROLE1");
   Serial1.print("AT+START");
+
+  serialTimer = 0;
 }
 
 void loop() { // run over and over
   if (Serial1.available() > 0) {
+    serialTimer = 0;
     Serial.write(Serial1.read());
+  }
+  else {
+    ++serialTimer;
+    if (serialTimer == 50000) {
+      Serial1.end();
+      Serial1.begin(115200);
+      while (!Serial1) {
+        ; // wait for serial1 port to connect. Needed for Bluetooth
+      }
+      // BT.print("AT+ROLE1");
+      Serial1.print("AT+START");
+
+      serialTimer = 0;
+    }
   }
   if (Serial.available() > 0) {
     Serial1.write(Serial.read());
