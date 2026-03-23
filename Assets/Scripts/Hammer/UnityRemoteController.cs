50e4df5ff10e4defd2474b43d4ca2de5e9d0f909
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-03-23 22:15:07 +0000

    more null checks
    structure is definitely weird

--- Assets/Scripts/Hammer/UnityRemoteController.cs
+++ Assets/Scripts/Hammer/UnityRemoteController.cs
@@ -8,31 +8,47 @@ namespace Hammer
     public class UnityRemoteController : IController
     {
         private AttitudeSensor _attitudeSensor;
+        private LinearAccelerationSensor _linearAccelerationSensor;
         
         public void Connect()
         {
+            Debug.Log("Attempting to connect to Unity Remote");
+            
             // Determine if a Gyroscope sensor device is present.
             // if (Gyroscope.current != null)
                 // Debug.Log("Gyroscope present");
             // InputSystem.EnableDevice(Gyroscope.current);
             
-            _attitudeSensor = InputSystem.GetDevice<AndroidRotationVector>();
+            _attitudeSensor = InputSystem.GetDevice<AndroidGameRotationVector>();
             if (_attitudeSensor == null)
             {
-                _attitudeSensor = InputSystem.GetDevice<AndroidGameRotationVector>();
+                _attitudeSensor = InputSystem.GetDevice<AndroidRotationVector>();
                 if (_attitudeSensor == null)
                 {
-                    Debug.LogError("AttitudeSensor is not available");
-                    _attitudeSensor = AttitudeSensor.current;
+                    _attitudeSensor = InputSystem.GetDevice<AttitudeSensor>();
+                    if (_attitudeSensor == null)
+                    {
+                        Debug.LogError("AttitudeSensor is not available");
+                        _attitudeSensor = AttitudeSensor.current;
+                    }
                 }
             }
-            
             if (_attitudeSensor != null)
             {
                 InputSystem.EnableDevice(_attitudeSensor);
             }
-            
-            InputSystem.EnableDevice(LinearAccelerationSensor.current);
+
+            _linearAccelerationSensor = InputSystem.GetDevice<LinearAccelerationSensor>();
+            if (_linearAccelerationSensor == null)
+            {
+                Debug.LogError("LinearAccelerationSensor is not available");
+                _linearAccelerationSensor = LinearAccelerationSensor.current;
+            }
+
+            if (_linearAccelerationSensor != null)
+            {
+                InputSystem.EnableDevice(_linearAccelerationSensor);
+            }
         }
 
         public void Update()
@@ -50,12 +66,15 @@ namespace Hammer
 
         public Vector3 GetAcceleration()
         {
-            return LinearAccelerationSensor.current.acceleration.ReadValue();
+            return _linearAccelerationSensor.acceleration.ReadValue();
         }
 
         public void Cleanup()
         {
-            InputSystem.DisableDevice(Gyroscope.current);
+            InputSystem.DisableDevice(_attitudeSensor);
+            InputSystem.DisableDevice(_linearAccelerationSensor);
+            _attitudeSensor = null;
+            _linearAccelerationSensor = null;
         }
     }
 }
