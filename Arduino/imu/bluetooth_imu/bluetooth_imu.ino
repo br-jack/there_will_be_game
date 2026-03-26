9866d278aa54cf60c6549fb94cb7a8c4d5159efb
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-03-26 13:47:28 +0000

    imu hardwareserial passes this test so it's probably good
    serial prints to only the usb, serial1 prints to only the bluetooth.

--- Arduino/imu/bluetooth_imu/bluetooth_imu.ino
+++ Arduino/imu/bluetooth_imu/bluetooth_imu.ino
@@ -27,56 +27,56 @@ void setup(void) {
   //   ; // wait for serial port to connect. Needed for native USB port only
   // }
 
-  // Serial1.begin(9600);
-  // Serial1.print("AT+ROLE0");
-  // Serial1.end();
-  Serial1.begin(115200);
+  // Serial.begin(9600);
+  // Serial.print("AT+ROLE0");
+  // Serial.end();
+  Serial.begin(115200);
 
-  while (!Serial1) delay(5);
+  while (!Serial) delay(5);
 
   delay(20);
 
-  Serial1.println("Adafruit BNO08x test!");
+  Serial.println("Adafruit BNO08x test!");
 
   // Try to initialize!
   if (!bno08x.begin_I2C()) {
-    //if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
+    //if (!bno08x.begin_UART(&Serial)) {  // Requires a device with > 300 byte UART buffer!
     //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
-    Serial1.println("Failed to find BNO08x chip");
+    Serial.println("Failed to find BNO08x chip");
     while (1) {
       delay(120); 
       //see https://github.com/adafruit/Adafruit_BNO08x/issues/34#issuecomment-2533685723
       rp2040.reboot();
     }
   }
-  Serial1.println("BNO08x Found!");
+  Serial.println("BNO08x Found!");
 
   for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
-    Serial1.print("Part ");
-    Serial1.print(bno08x.prodIds.entry[n].swPartNumber);
-    Serial1.print(": Version :");
-    Serial1.print(bno08x.prodIds.entry[n].swVersionMajor);
-    Serial1.print(".");
-    Serial1.print(bno08x.prodIds.entry[n].swVersionMinor);
-    Serial1.print(".");
-    Serial1.print(bno08x.prodIds.entry[n].swVersionPatch);
-    Serial1.print(" Build ");
-    Serial1.println(bno08x.prodIds.entry[n].swBuildNumber);
+    Serial.print("Part ");
+    Serial.print(bno08x.prodIds.entry[n].swPartNumber);
+    Serial.print(": Version :");
+    Serial.print(bno08x.prodIds.entry[n].swVersionMajor);
+    Serial.print(".");
+    Serial.print(bno08x.prodIds.entry[n].swVersionMinor);
+    Serial.print(".");
+    Serial.print(bno08x.prodIds.entry[n].swVersionPatch);
+    Serial.print(" Build ");
+    Serial.println(bno08x.prodIds.entry[n].swBuildNumber);
   }
   setReports();
 
-  Serial1.println("Reading events");
+  Serial.println("Reading events");
   delay(100);
 }
 
 // Here is where you define the sensor outputs you want to receive
 void setReports(void) {
-  Serial1.println("Setting desired reports");
+  Serial.println("Setting desired reports");
   if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
-    Serial1.println("Could not enable game vector");
+    Serial.println("Could not enable game vector");
   }
   if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION, 20000)) {
-    Serial1.println("Could not enable linear acceleration");
+    Serial.println("Could not enable linear acceleration");
   }
 }
 
@@ -85,20 +85,20 @@ void loop() {  // run over and over
 
   delay(5);
 
-  // if (Serial1.available()) {
-  //   Serial.write(Serial1.read());
+  // if (Serial.available()) {
+  //   Serial.write(Serial.read());
   // }
   // if (Serial.available()) {
-  //   Serial1.write(Serial.read());
+  //   Serial.write(Serial.read());
   // }
 
   if (bno08x.wasReset()) {
-    Serial1.print("sensor was reset ");
+    Serial.print("sensor was reset ");
     setReports();
   }
 
   if (!bno08x.getSensorEvent(&sensorValue)) {
-    //Serial1.println("Unable to get sensor event!");
+    //Serial.println("Unable to get sensor event!");
     return;
   }
   switch (sensorValue.sensorId) {
@@ -116,7 +116,7 @@ void loop() {  // run over and over
         q.concat(sensorValue.un.gameRotationVector.k);
 
 
-        Serial1.println(q);
+        Serial.println(q);
         break;
       }
 
@@ -130,7 +130,7 @@ void loop() {  // run over and over
         q.concat(sensorValue.un.linearAcceleration.y);
         q.concat(":");
         q.concat(sensorValue.un.linearAcceleration.z);
-        Serial1.println(q);
+        Serial.println(q);
         break;
       }
   }
