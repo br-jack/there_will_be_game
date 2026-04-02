d4de18d277d330120358a6c566656b4aab060ed6
br-jack <brewer.j-ack@protonmail.com>
2026-04-02 15:20:41 +0100

    undid that lol

--- Assets/Scripts/UI/ScoreMeters/AweMeterUI.cs
+++ Assets/Scripts/UI/ScoreMeters/AweMeterUI.cs
@@ -4,12 +4,6 @@ namespace Score
 {
     public class AweMeterUI : IMeterUI
     {
-        [SerializeField] protected new int max = 5000;
-        [SerializeField] protected new float fullBarWidth = 280.0f;
-
-        [SerializeField] protected new Color lowScoreColor = new Color(1.0f, 0.72f, 0.15f, 1.0f);
-        [SerializeField] protected new Color midScoreColor = new Color(1.0f, 0.45f, 0.10f, 1.0f);
-        [SerializeField] protected new Color highScoreColor = new Color(0.90f, 0.15f, 0.15f, 1.0f);
         override protected void Start()
         {
             if (ScoreManager.Instance != null)
