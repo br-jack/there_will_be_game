5a4a3d599e83d6bc6ed5b15fc1466d5706d3ee65
Edward Boileau <edwardboileau@gmail.com>
2026-04-03 12:38:02 +0100

    added thing to check the forwards speed of the hammer head, sadly doesn't work

--- Assets/hammerParticleSystemBehaviour.cs
+++ Assets/hammerParticleSystemBehaviour.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class hammerParticleSystemBehaviour : MonoBehaviour
 {
     private float headSpeedForwards;
+
     private Transform _tf;
     private Vector3 posPrevFrame;
 
+    public float trailSpeedThreshold;
+    private ParticleSystem _ps;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        /*
+        
         _tf = GetComponent<Transform>();
+        _ps = GetComponent<ParticleSystem>();
         posPrevFrame = _tf.position;
-        */
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
+        
         Vector3 positionChange = _tf.position - posPrevFrame;
         Vector3 velocity = positionChange/Time.deltaTime;
         Vector3 headForward = _tf.forward; //normalised forward vector from head of hammer
@@ -29,7 +33,12 @@ public class hammerParticleSystemBehaviour : MonoBehaviour
         //find scalar speed of hammer head in the forwards direction
         headSpeedForwards = Vector3.Magnitude(headForward * Vector3.Dot(headForward,velocity));
         Debug.Log("headSpeedForwards: "+headSpeedForwards);
-        */
+
+        if (headSpeedForwards > trailSpeedThreshold) _ps.Play();
+        //else _ps.Stop();
+        
+        posPrevFrame = _tf.position;
+        
 
     }
 }
