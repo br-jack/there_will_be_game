2ef2e782f742d9d4ad62404cc6efdb8adf044a1e
Edward Boileau <edwardboileau@gmail.com>
2026-04-05 12:33:07 +0100

    broke everything with references to null particle system modules

--- Assets/Scripts/Player/Hammer/hammerStateEffects.cs
+++ Assets/Scripts/Player/Hammer/hammerStateEffects.cs
@@ -4,42 +4,78 @@ public class hammerStateEffects : MonoBehaviour
 {
     public hammerSpeedState hammerSpeedState;
     public hammerChargeState hammerChargeState;
-    private ParticleSystem.MainModule _mmod;
+
+    
+    
+    
+    public ParticleSystem embers;
+    private ParticleSystem.EmissionModule _embersEmission;
+    private ParticleSystem.MinMaxGradient _embersCOLGradient;
+
+    public ParticleSystem chargeLines;
+    private ParticleSystem.EmissionModule _chargeLinesEmission;
+    private ParticleSystem.MainModule _chargeLinesMain;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _chargeLinesMain = chargeLines.main;
+        _chargeLinesEmission = chargeLines.emission;
+        _embersEmission = embers.emission;
+        _embersCOLGradient = embers.colorOverLifetime.color;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        /*
         switch (hammerChargeState)
         {
-            default: break;
+            case hammerChargeState.uncharged:
+                _embersEmission.enabled = false;
+                _chargeLinesEmission.enabled = false;
+                break;
+            case hammerChargeState.charging: 
+                _embersEmission.enabled = false;
+                _chargeLinesEmission.enabled = true; 
+                break;
+            case hammerChargeState.charged: 
+                _embersEmission.enabled = true;
+                _chargeLinesEmission.enabled = true; 
+                break;
         }
+        */
         switch (hammerSpeedState) {
             case hammerSpeedState.still: 
-                _mmod.startColor = Color.white;
+                _chargeLinesMain.startColor = Color.white;
+                _embersCOLGradient.colorMin = Color.white;
                 break;
             case hammerSpeedState.trotting: 
-                _mmod.startColor = Color.blue;
+                _chargeLinesMain.startColor = Color.blue;
+                _embersCOLGradient.colorMin = Color.blue;
                 break;
             case hammerSpeedState.cantering: 
-                _mmod.startColor = Color.yellow;
+                _chargeLinesMain.startColor = Color.yellow;
+                _embersCOLGradient.colorMin = Color.yellow;
                 break;
             case hammerSpeedState.galloping: 
-                _mmod.startColor = Color.red;
+                _chargeLinesMain.startColor = Color.red;
+                _embersCOLGradient.colorMin = Color.red;
                 break;
             case hammerSpeedState.ultraGalloping: 
-                _mmod.startColor = Color.magenta;
+                _chargeLinesMain.startColor = Color.magenta;
+                _embersCOLGradient.colorMin = Color.magenta;
                 break;
             case hammerSpeedState.vulcan: 
-                _mmod.startColor = Color.white;
+                _chargeLinesMain.startColor = Color.white;
+                _embersCOLGradient.colorMin = Color.white;
                 break;
             default: 
                 break;
+            
         }
+        //something like this may be necessary, not sure how well the pointers work
+        //_chargeLinesCOL.color = _embersCOLGradient; 
     }
 }
