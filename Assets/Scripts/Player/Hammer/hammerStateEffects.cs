1bb33e6d28488f23990531a93d71036687426b80
Edward Boileau <edwardboileau@gmail.com>
2026-04-05 20:31:08 +0100

    stupid colours don't work. i dont understand why this is but i will solve it later

--- Assets/Scripts/Player/Hammer/hammerStateEffects.cs
+++ Assets/Scripts/Player/Hammer/hammerStateEffects.cs
@@ -23,7 +23,7 @@ public class hammerStateEffects : MonoBehaviour
     public ParticleSystem ghosts;
     private ParticleSystem.MainModule _ghostsMain;
 
-    private Color _effectColor;
+    //public Color _effectColor;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -62,35 +62,59 @@ public class hammerStateEffects : MonoBehaviour
         
         switch (hammerSpeedState) {
             case hammerSpeedState.still: 
-                _effectColor = Color.white;
+                _chargeLinesMain.startColor = Color.white; 
+                _embersMain.startColor = Color.white;
+                _glowMain.startColor = Color.white;
+                _ghostsMain.startColor = Color.white;
+                _trailsMain.startColor = Color.white;
                 break;
             case hammerSpeedState.walking: 
-                _effectColor = Color.green;
+                _chargeLinesMain.startColor = Color.green; 
+                _embersMain.startColor = Color.green;
+                _glowMain.startColor = Color.green;
+                _ghostsMain.startColor = Color.green;
+                _trailsMain.startColor = Color.green;
                 break;
             case hammerSpeedState.trotting: 
-                _effectColor = Color.blue;
+                _chargeLinesMain.startColor = Color.blue; 
+                _embersMain.startColor = Color.blue;
+                _glowMain.startColor = Color.blue;
+                _ghostsMain.startColor = Color.blue;
+                _trailsMain.startColor = Color.blue;
                 break;
             case hammerSpeedState.cantering: 
-                _effectColor = Color.yellow;
+                _chargeLinesMain.startColor =  Color.yellow;
+                _embersMain.startColor = Color.yellow;
+                _glowMain.startColor = Color.yellow;
+                _ghostsMain.startColor = Color.yellow;
+                _trailsMain.startColor = Color.yellow;
                 break;
             case hammerSpeedState.galloping: 
-                _effectColor = Color.red;
+                _chargeLinesMain.startColor = Color.red; 
+                _embersMain.startColor = Color.red;
+                _glowMain.startColor = Color.red;
+                _ghostsMain.startColor = Color.red;
+                _trailsMain.startColor = Color.red;
                 break;
             case hammerSpeedState.ultraGalloping: 
-                _effectColor = Color.magenta;
+                _chargeLinesMain.startColor =  Color.magenta;
+                _embersMain.startColor = Color.magenta;
+                _glowMain.startColor = Color.magenta;
+                _ghostsMain.startColor = Color.magenta;
+                _trailsMain.startColor = Color.magenta;
                 break;
             case hammerSpeedState.vulcan: 
-                _effectColor = Color.black;
+                _chargeLinesMain.startColor = Color.black; 
+                _embersMain.startColor = Color.black;
+                _glowMain.startColor = Color.black;
+                _ghostsMain.startColor = Color.black;
+                _trailsMain.startColor = Color.black;
                 break;
             default: 
                 break;
             
         }
-        _chargeLinesMain.startColor = _effectColor;
-        _embersMain.startColor = _effectColor;
-        _glowMain.startColor = _effectColor;
-        _ghostsMain.startColor = _effectColor;
-        _trailsMain.startColor = _effectColor;
+        
         //something like this may be necessary, not sure how well the pointers work
         //_chargeLinesCOL.color = _embersCOLGradient; 
     }
