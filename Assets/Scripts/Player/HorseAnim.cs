55bd7ec8ed4f984f4ab40f70a1f8ae5f5e24f069
HaSRoberts <tu23666@bristol.ac.uk>
2026-04-06 15:15:07 +0100

    got very basic movement working

--- Assets/Scripts/Player/HorseAnim.cs
+++ Assets/Scripts/Player/HorseAnim.cs
@@ -6,27 +6,42 @@ public class HorseAnim : MonoBehaviour
 {
     public float speed;
     Animator horseAnimator;
+    public GameObject horseMovObj;
+    HorseMovement horseMovement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         horseAnimator = GetComponent<Animator>();
+        horseMovement = horseMovObj.GetComponent<HorseMovement>();
+        if (horseAnimator.enabled) //this is here because the animator panel is bugged in this unity version :(
+        {
+        horseAnimator.Rebind();
+        horseAnimator.Update(0);
+        } 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if(horseAnimator != null)
         {
-            Debug.LogError("horszz");
-            if (speed > 10)
+            speed = horseMovement.GetCurrentSpeed();
+            horseAnimator.SetFloat("Speed", speed);
+            //Debug.LogError("horszz");
+          
+            if (speed  > 15)
             {
                 horseAnimator.SetTrigger("Gallop3");
-                Debug.LogError("hors");
-
+                horseAnimator.ResetTrigger("Gallop0");
+                //Debug.LogError("horszz");
             }
-            if (speed > 20)
+            else if (speed > 5 && speed < 13)
             {
                 horseAnimator.SetTrigger("Gallop0");
+                horseAnimator.ResetTrigger("Gallop3");
+                
+                //Debug.LogError("hors");
             }
         }
         else
