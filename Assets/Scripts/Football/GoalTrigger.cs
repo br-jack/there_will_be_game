9dfa33d556d5bd55edce42df48c0863fc92e7230
Jack-Roddam <roddamjb@outlook.com>
2026-04-07 15:28:59 +0100

    basic football task (for nike?) ball cna be kicked by player and enemies, and registers a goal in the enemy goal, no reward or specific enemy team yet

---
+++ Assets/Scripts/Football/GoalTrigger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Football")) return;
+        Football ball = other.GetComponent<Football>();
+        
+        if (ball != null)
+        {
+            FootballTask task = FindFirstObjectByType<FootballTask>();
+            if (task != null) task.goalScored();
+            ball.StartCoroutine(ball.ResetBall(1.5f));
+            Debug.Log("GOAL SCORED!");
+        }
+    }
+}
