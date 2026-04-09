9358af5c671aa92cab847ec74b23c509470cba70
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-04-09 18:59:28 +0100

    test i2c scan
    everything as expected. one i2c address on Wire for the IMU, nothing on wire1
    
    https://learn.adafruit.com/scanning-i2c-addresses/arduino

--- Arduino/imu/bluetooth_imu/bluetooth_imu.ino
+++ Arduino/imu/bluetooth_imu/bluetooth_imu.ino
@@ -1,312 +1,64 @@
-// Basic demo for readings from Adafruit BNO08x
-// Install this manually
-#include <Adafruit_BNO08x.h>
-#include <string.h>
+// SPDX-FileCopyrightText: 2023 Carter Nelson for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+// --------------------------------------
+// i2c_scanner
+//
+// Modified from https://playground.arduino.cc/Main/I2cScanner/
+// --------------------------------------
 
-// For SPI mode, we need a CS pin
-#define BNO08X_CS 10
-#define BNO08X_INT 9
+#include <Wire.h>
 
-// For SPI mode, we also need a RESET
-// #define BNO08X_RESET 5
-// but not for I2C or UART
-#define BNO08X_RESET -1
-#define FAST_MODE
+// Set I2C bus to use: Wire, Wire1, etc.
+#define WIRE Wire1
 
-constexpr int motor1DIRPin = A3;  // Motor 1 Direction pin of dual motor driver connected to digital pin 29
-constexpr int motor1SPDPin = A2;  // Motor 1 Speed PWM pin of dual motor driver connected to digital pin 28
-constexpr int motor2SPDPin = A1;  // Motor 2 Speed PWM pin of dual motor driver connected to digital pin 27
-constexpr int motor2DIRPin = A0;  // Motor 2 Direction pin of dual motor driver connected to digital pin 26
+void setup() {
+  WIRE.begin();
 
-unsigned long rumbleStartMs;
-unsigned long rumbleDuration;
-int rumbleStrength = 255;
-
-int rumbleCurrentFadeMs;
-constexpr int rumbleFadeInterval = 30;
-
-enum class RumbleMode {
-  Off,
-  Constant,
-  RampUp,
-  RampDown,
-};
-
-RumbleMode currentRumbleMode { RumbleMode::Off };
-
-Adafruit_BNO08x bno08x(BNO08X_RESET);
-sh2_SensorValue_t sensorValue;
-
-void setup(void) {
-  //Without this inital delay it will usually never connect until you press the reset button
-  delay(100);
-
-  // Open serial communications and wait for port to open:
-  // Serial.begin(115200);
-  // while (!Serial) {
-  //   ; // wait for serial port to connect. Needed for native USB port only
-  // }
-
-  // Serial1.begin(9600);
-  // Serial1.print("AT+ROLE0");
-  // Serial1.end();
-  Serial1.begin(115200);
-
-  while (!Serial1) delay(5);
-
-  delay(20);
-
-  Serial1.println(F("info:Adafruit BNO08x test!"));
-
-  // Try to initialize!
-  if (!bno08x.begin_I2C()) {
-    //if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
-    //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
-    Serial1.println(F("info:Failed to find BNO08x chip"));
-    while (1) {
-      delay(120); 
-      //see https://github.com/adafruit/Adafruit_BNO08x/issues/34#issuecomment-2533685723
-      rp2040.reboot();
-    }
-  }
-  Serial1.println(F("info:BNO08x Found!"));
-
-  for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
-    Serial1.print("Part ");
-    Serial1.print(bno08x.prodIds.entry[n].swPartNumber);
-    Serial1.print(": Version :");
-    Serial1.print(bno08x.prodIds.entry[n].swVersionMajor);
-    Serial1.print(".");
-    Serial1.print(bno08x.prodIds.entry[n].swVersionMinor);
-    Serial1.print(".");
-    Serial1.print(bno08x.prodIds.entry[n].swVersionPatch);
-    Serial1.print(" Build ");
-    Serial1.println(bno08x.prodIds.entry[n].swBuildNumber);
-  }
-  setReports();
-
-  Serial1.println(F("info:Reading events"));
-
-  pinMode(motor1DIRPin, OUTPUT);
-  pinMode(motor2DIRPin, OUTPUT);
-
-  digitalWrite(motor1DIRPin, HIGH);
-
-  delay(100);
+  Serial.begin(9600);
+  while (!Serial)
+     delay(10);
+  Serial.println("\nI2C Scanner");
 }
 
-// Here is where you define the sensor outputs you want to receive
-void setReports(void) {
-  Serial1.println(F("info:Setting desired reports"));
-  if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
-    Serial1.println(F("info:Could not enable game vector"));
-  }
-  if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION, 20000)) {
-    Serial1.println(F("info:Could not enable linear acceleration"));
-  }
 
-  //https://github.com/sparkfun/SparkFun_BNO08x_Arduino_Library/issues/2
-  //delay(100); // This delay allows enough time for the BNO085 to accept the new configuration and clear its reset status
-}
+void loop() {
+  byte error, address;
+  int nDevices;
 
-void startRumble(RumbleMode mode, int duration) {
-  // fade in from min to max in increments of 5 points:
-  // for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5) {
-    // sets the value (range from 0 to 255):
-    // wait for 30 milliseconds to see the dimming effect
-    // delay(120);
-  // }
+  Serial.println("Scanning...");
 
-  // fade out from max to min in increments of 5 points:
-  // for (int fadeValue = 255; fadeValue >= 0; fadeValue -= 5) {
-    // sets the value (range from 0 to 255):
-    // analogWrite(motor1SPDPin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    // delay(120);
-  // }
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    WIRE.beginTransmission(address);
+    error = WIRE.endTransmission();
 
-  currentRumbleMode = mode;
-  rumbleStartMs = millis();
-  //NOTE: assume duration is unsigned
-  rumbleDuration = duration;
-  if (mode == RumbleMode::RampUp) {
-    rumbleStrength = 0;
-  }
-  else {
-    rumbleStrength = 255;
-  }
-  rumbleCurrentFadeMs = 0;
-  Serial1.println(F("info:Rumble activated."));
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
 
-  analogWrite(motor1SPDPin, rumbleStrength);
-}
-
-void endRumble(void) {
-  // fade in from min to max in increments of 5 points:
-  // for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5) {
-    // sets the value (range from 0 to 255):
-    // wait for 30 milliseconds to see the dimming effect
-    // delay(120);
-  // }
-
-  // fade out from max to min in increments of 5 points:
-  // for (int fadeValue = 255; fadeValue >= 0; fadeValue -= 5) {
-    // sets the value (range from 0 to 255):
-    // analogWrite(motor1SPDPin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    // delay(120);
-  // }
-
-  rumbleStartMs = 0;
-  rumbleDuration = 0;
-  currentRumbleMode = RumbleMode::Off;
-  rumbleCurrentFadeMs = 0;
-  Serial1.println(F("info:Rumble deactivated."));
-
-  analogWrite(motor1SPDPin, 0);
-}
-
-inline void outputSensorValues(void) {
-  if (bno08x.wasReset()) {
-    Serial1.print(F("info:Sensor was reset "));
-    setReports();
-  }
-
-  if (!bno08x.getSensorEvent(&sensorValue)) {
-    //Serial1.println("info:Unable to get sensor event!");
-    return;
-  }
-  switch (sensorValue.sensorId) {
-
-    case SH2_GAME_ROTATION_VECTOR:
-      {
-        String q;
-        q.concat("q:");
-        q.concat(sensorValue.un.gameRotationVector.real);
-        q.concat(":");
-        q.concat(sensorValue.un.gameRotationVector.i);
-        q.concat(":");
-        q.concat(sensorValue.un.gameRotationVector.j);
-        q.concat(":");
-        q.concat(sensorValue.un.gameRotationVector.k);
-
-
-        Serial1.println(q);
-        break;
-      }
-
-
-    case SH2_LINEAR_ACCELERATION:
-      {
-        String q;
-        q.concat("a:");
-        q.concat(sensorValue.un.linearAcceleration.x);
-        q.concat(":");
-        q.concat(sensorValue.un.linearAcceleration.y);
-        q.concat(":");
-        q.concat(sensorValue.un.linearAcceleration.z);
-        Serial1.println(q);
-        break;
-      }
-  }
-}
-
-void loop(void) {  // run over and over
-
-  delay(5);
-
-  outputSensorValues();
-
-  if (Serial1.available() > 1) {
-    constexpr char rumbleChar = 'R';
-
-    const int incomingByte = Serial1.read();
-
-    //rumble string format: "RMx\n" where M is the mode and x is the duration in ms
-    if (incomingByte == (int)rumbleChar) {
-
-      const int modeByte = Serial1.read();
-
-      RumbleMode mode;
-
-      switch(modeByte) {
-        case (int)'C': {
-          mode = RumbleMode::Constant;
-          break;
-        }
-        case (int)'U': {
-          mode = RumbleMode::RampUp;
-          break;
-        }
-        case (int)'D': {
-          mode = RumbleMode::RampDown;
-          break;
-        }
-        default: {
-          mode = RumbleMode::Constant;
-          break;
-        }
-      }
-
-      const int duration = Serial1.parseInt();
-
-      //Serial.println(duration);  
-      startRumble(mode, duration);
+      nDevices++;
     }
-    
-    //discard newline character and any other remaining characters in buffer
-    (void) Serial1.readString();
-  }
-
-  if (currentRumbleMode != RumbleMode::Off) {
-    const unsigned long currentMs = millis();
-
-    //handle millis wrap around (unlikely to occur)
-    if (currentMs < rumbleStartMs) {
-      rumbleStartMs = 0;
-    }
-
-    if ((currentMs - rumbleStartMs) >= rumbleDuration) {
-      endRumble();
-    }
-    else {
-      switch(currentRumbleMode) {
-        case RumbleMode::RampUp: {
-          //also handle millis wrap around
-          if (rumbleCurrentFadeMs == 0 || currentMs < rumbleCurrentFadeMs) {
-            rumbleCurrentFadeMs = currentMs;
-          }
-          else if ((currentMs - rumbleCurrentFadeMs) >= rumbleFadeInterval) {
-            rumbleCurrentFadeMs = currentMs;
-            if (rumbleStrength < 255) {
-              rumbleStrength++;
-              analogWrite(motor1SPDPin, rumbleStrength);
-            }
-          }
-
-          break;
-        }
-
-        case RumbleMode::RampDown: {
-          //also handle millis wrap around
-          if (rumbleCurrentFadeMs == 0 || currentMs < rumbleCurrentFadeMs) {
-            rumbleCurrentFadeMs = currentMs;
-          }
-          else if ((currentMs - rumbleCurrentFadeMs) >= rumbleFadeInterval) {
-            rumbleCurrentFadeMs = currentMs;
-            if (rumbleStrength > 0) {
-              rumbleStrength -= 5;
-              analogWrite(motor1SPDPin, rumbleStrength);
-            }
-          }
-
-          break;
-        }
-      }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
     }
   }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
 
-  // if (Serial.available()) {
-  //   Serial1.write(Serial.read());
-  // }
+  delay(5000);           // wait 5 seconds for next scan
 }
