b4435e98a15c0f77161345f190213aa253b5663e
Jack-Roddam <roddamjb@outlook.com>
2026-04-09 18:32:44 +0100

    made game easier - put goals on edge, added fireworks VFX/SFX on scoring

--- Assets/Scripts/Football/GoalTrigger.cs
+++ Assets/Scripts/Football/GoalTrigger.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class GoalTrigger : MonoBehaviour
 {
+    public ParticleSystem confetti;
+    public ParticleSystem fireworks;
+    public AudioClip fireworksSFX;
+    [Range(0, 1)] public float volume = 1.0f;
+
+    void Awake()
+    {
+       fireworksSFX = Resources.Load<AudioClip>("fireworksSFX"); 
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!other.gameObject.CompareTag("Football")) return;
@@ -9,11 +18,25 @@ public class GoalTrigger : MonoBehaviour
         
         if (ball != null)
         {
+            if (confetti != null)
+            {
+                confetti.Play();
+            }
+
+            if (fireworks != null && fireworksSFX != null)
+            {
+                AudioSource.PlayClipAtPoint(fireworksSFX, transform.position, volume);
+                fireworks.Play();
+            }
+
             FootballTask task = FindFirstObjectByType<FootballTask>();
             if (task != null) task.goalScored();
+
             ball.StartCoroutine(ball.ResetBall(1.5f));
+
             PitchManager pitchManager = FindFirstObjectByType<PitchManager>();
             pitchManager.EndFootballMiniGame();
+
             Debug.Log("GOAL SCORED!");
         }
     }
