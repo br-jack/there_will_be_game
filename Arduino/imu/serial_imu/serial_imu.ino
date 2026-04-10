45a84e00142184a149a57dd11e6e3b6eb4b95264
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-04-10 15:33:43 +0100

    use similar code in serial_imu minus the rumble (and obviously bluetooth)
    going to build up from fewer parts to adding more to find the issue.
    
    i think this is more stable than the  soldered one for some reason (trying on breadboard wit hjust imu and rp2040 rn). i wonder if it has to do with the cable length? the soldered one uses two cables and the strip to connect the SCL and SDA cables

--- Arduino/imu/serial_imu/serial_imu.ino
+++ Arduino/imu/serial_imu/serial_imu.ino
@@ -1,4 +1,3 @@
-#include <SoftwareSerial.h>
 // Basic demo for readings from Adafruit BNO08x
 // Install this manually
 #include <Adafruit_BNO08x.h>
@@ -17,28 +16,34 @@
 Adafruit_BNO08x bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
 
-SoftwareSerial BT(D8, D9);  // RX, TX
-
 void setup(void) {
   //Without this inital delay it will usually never connect until you press the reset button
   delay(100);
 
-//  / Open serial communications and wait for port to open:
-   Serial.begin(115200);
-   while (!Serial) {
-     ; // wait for serial port to connect. Needed for native USB port only
-   }
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
-  Serial.println("Adafruit BNO08x test!");
+  while (!Serial) delay(5);
+
+  delay(20);
+
+  Serial.println(F("info:Adafruit BNO08x test!"));
 
   // Try to initialize!
   if (!bno08x.begin_I2C()) {
-    //if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
+    //if (!bno08x.begin_UART(&Serial)) {  // Requires a device with > 300 byte UART buffer!
     //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
-    Serial.println("Failed to find BNO08x chip");
-    while (1) { delay(5); }
+    Serial.println(F("info:Failed to find BNO08x chip"));
+    while (1) {
+      delay(120); 
+      //see https://github.com/adafruit/Adafruit_BNO08x/issues/34#issuecomment-2533685723
+      rp2040.reboot();
+    }
   }
-  Serial.println("BNO08x Found!");
+  Serial.println(F("info:BNO08x Found!"));
 
   for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
     Serial.print("Part ");
@@ -54,32 +59,33 @@ void setup(void) {
   }
   setReports();
 
-  Serial.println("Reading events");
+  Serial.println(F("info:Reading events"));
+
   delay(100);
 }
 
 // Here is where you define the sensor outputs you want to receive
 void setReports(void) {
-  Serial.println("Setting desired reports");
+  Serial.println(F("info:Setting desired reports"));
   if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
-    Serial.println("Could not enable game vector");
+    Serial.println(F("info:Could not enable game vector"));
   }
   if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION, 20000)) {
-    Serial.println("Could not enable linear acceleration");
+    Serial.println(F("info:Could not enable linear acceleration"));
   }
+
+  //https://github.com/sparkfun/SparkFun_BNO08x_Arduino_Library/issues/2
+  //delay(100); // This delay allows enough time for the BNO085 to accept the new configuration and clear its reset status
 }
 
-
-void loop() {  // run over and over
-
-  delay(5);
-  
+inline void outputSensorValues(void) {
   if (bno08x.wasReset()) {
-    Serial.print("sensor was reset ");
+    Serial.print(F("info:Sensor was reset "));
     setReports();
   }
 
   if (!bno08x.getSensorEvent(&sensorValue)) {
+    //Serial.println("info:Unable to get sensor event!");
     return;
   }
   switch (sensorValue.sensorId) {
@@ -115,4 +121,15 @@ void loop() {  // run over and over
         break;
       }
   }
+}
+
+void loop(void) {  // run over and over
+
+  delay(5);
+
+  outputSensorValues();
+
+  // if (Serial.available()) {
+  //   Serial.write(Serial.read());
+  // }
 }
