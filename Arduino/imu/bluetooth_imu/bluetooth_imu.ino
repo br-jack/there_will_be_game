1e7f1ebf232ea061b22e3e91468e3d3fc8211c0d
speedranked6351 <126418929+speedranked6351@users.noreply.github.com>
2026-04-14 00:25:22 +0100

    FIX by using serial.read rather than serial.readstring to empty buffer
    i guess the string overhead made it take longer?? either that or the read timeout

--- Arduino/imu/bluetooth_imu/bluetooth_imu.ino
+++ Arduino/imu/bluetooth_imu/bluetooth_imu.ino
@@ -253,10 +253,10 @@ void loop(void) {  // run over and over
       startRumble(mode, duration);
     }
     
-    // if (Serial.available() > 0) {
-    //   //discard newline character and any other remaining characters in buffer
-    //   (void) Serial.readString();
-    // }
+    while (Serial.available() > 0) {
+      //discard newline character and any other remaining characters in buffer
+      (void) Serial.read();
+    }
   }
 
   if (currentRumbleMode != RumbleMode::Off) {
