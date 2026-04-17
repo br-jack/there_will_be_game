4b23c5d3835ba8934ffe7b94e74299de9c9ed601
Aravind Pillai <iv23317@bristol.ac.uk>
2026-04-17 13:17:13 +0100

    The task to go to the point appears on the panel when the panel appears

--- Assets/Scripts/Tutorial/TutorialReachPointTask.cs
+++ Assets/Scripts/Tutorial/TutorialReachPointTask.cs
@@ -2,23 +2,13 @@ using UnityEngine;
 
 public class TutorialReachPointTask : BaseTask
 {
-    [SerializeField] private string tutorialTaskName = "Approach the marked area";
-    [SerializeField] private string tutorialTaskDescription = "Walk to the glowing marker";
-
     private bool playerReachedGoal = false;
 
-    private void Awake()
+    private void Start()
     {
-        taskName = tutorialTaskName;
-        taskDescription = tutorialTaskDescription;
-    }
-
-    public override void StartTask()
-    {
-        playerReachedGoal = false;
-        taskName = tutorialTaskName;
-        taskDescription = tutorialTaskDescription;
-        base.StartTask();
+        taskName = "Approach the marked area";
+        taskDescription = "Walk to the glowing marker";
+        StartTask();
     }
 
     public void MarkGoalReached()
