5efd379d445e3b00aaa5230c95069497b9fdb0f2
onettonett <157602827+onettonett@users.noreply.github.com>
2026-04-18 18:24:00 +0100

    CivilianAI initiated

---
+++ Assets/Scripts/Civilian/CivilianAI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class CivilianAI : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
