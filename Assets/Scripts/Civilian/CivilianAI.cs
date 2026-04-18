e6adef6d30aa3a8e1e38dd434efd170af7024b74
onettonett <157602827+onettonett@users.noreply.github.com>
2026-04-18 18:45:11 +0100

    run away and random movement settings

--- Assets/Scripts/Civilian/CivilianAI.cs
+++ Assets/Scripts/Civilian/CivilianAI.cs
@@ -4,6 +4,24 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(Rigidbody))]
 
+public struct RandomMovementSettings
+{
+    public float radius;
+    public float minInterval;
+    public float maxInterval;
+    public float speed;
+}
+
+public struct RunAwaySettings
+{
+    public float startRunningRadius;
+    public float stopRunningRadius;
+    public float speed;
+    public float targetDistance;
+    public float refreshInterval;
+
+}
+
 public class CivilianAI : MonoBehaviour
 {
     void Start()
