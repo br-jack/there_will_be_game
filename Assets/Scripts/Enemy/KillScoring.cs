c53634c116179e857d6042a67b1db8e06631cbb4
onettonett <157602827+onettonett@users.noreply.github.com>
2026-04-18 18:11:57 +0100

    added kill scoring file

---
+++ Assets/Scripts/Enemy/KillScoring.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Score
+{
+    public static class KillScoring
+    {
+        public static List<ScoreComponent> BuildKillScore(int baseScore, bool hasShield)
+        {
+            var components = new List<ScoreComponent>();
+            return components;
+        }
+    }
+}
