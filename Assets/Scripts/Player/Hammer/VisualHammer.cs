daebe35fad760007a7b8497b109809f8ea0428ca
br-jack <sr23140@bristol.ac.uk>
2026-04-28 15:11:14 +0100

    remove hammer headd

--- Assets/Scripts/Player/Hammer/VisualHammer.cs
+++ Assets/Scripts/Player/Hammer/VisualHammer.cs
@@ -91,41 +91,6 @@ namespace Hammer
                 }
             }
 
-            /*
-            Thoughts and notes:
-            - There should be a separate mode which makes the hammer more powerful and shiny and stuff.
-                - Maybe this is with awe maybe not
-                - This should have to be active to destroy buildings
-                - It should do an explosion in an area
-                - It should fling you upwards
-            - If the hammer has significant velocity (maybe rotational too) the horse should be effected
-                (like a dash)
-            - The target hammer should be update. maybe both are smaller which would let us use the 
-                bungeeing more and would maybe be more fun (though maybe we can scrap it considering how massive
-                the hammers gonne be anyways)
-                - We should deffo use a small spring system instead of teleporting. This would reduce the jitter
-                    and allow us to add animations
-                - Yeah yeah the way I did it didn't work we can do it though cause now were not bothing with full]
-                    collisions
-            - The hitbox size should be continuious with whatever velocity we use
-                - I dont even know if were going to use the linear acceleration data from the IMU
-
-            Order of operations (TODO):
-                1. Decide on the final size of the hammer/ update target hammer movement
-                2. Get some actual velocity value that we use (probably a combination of rotation and acceleration)
-                3. Use that velocity for the hammer rigid body (even if we teleport it otherwise)
-                4. Use velocity for collider size
-                5. Use spring system to make visual hammer movement a little nicer
-                6. Make a powered mode
-                7. Disable building destruction outside powered mode
-                8. Slams....
-                -. Implement velocity dash
-
-            Actually had a thought about the visual hammer bungeeing: we should just to all the bungee calculations (specifically)
-            the force stuff) ourselves then teleport it still. Trust me.
-
-            */
-
             // TODO this should probably be continuous
             if (head.forwardSpeed < mediumHitboxThreshold)
             {
@@ -154,5 +119,18 @@ namespace Hammer
         {
             //_targetHammer.Rumble();
         }
+
+            public void doSlam()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, slamRadius);
+        foreach (Collider c in colliders)
+        {
+            if (c.GetComponentInParent<StandardEnemyAI>())
+            {
+                Vector3 knockbackDirection = c.ClosestPoint(transform.position) - transform.position; //knock away
+                c.GetComponentInParent<StandardEnemyAI>().getKilledBasic(knockbackDirection * slamKnockbackAmount);
+            }
+        }
+    }
     }
 }
